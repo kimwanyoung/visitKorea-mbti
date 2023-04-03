31d5f27dff7cb12bcc044f7fb01bc18ee47b35fd
kimwanyoung <dhks2869@gmail.com>
2023-04-03 17:06:51 +0900

    add : getStaticProps

--- pages/resultPlace/[id].tsx
+++ pages/resultPlace/[id].tsx
@@ -4,42 +4,44 @@ import styled from "@emotion/styled"
 import Image from "next/image"
 import React, { useEffect } from "react"
 import GetMbtiPlace, { ResultPlaceI, RESULT_PLACE } from "@/lib/get-mbti-place"
-import { GetStaticProps } from "next"
+import { GetStaticProps, InferGetStaticPropsType } from "next"
 import { addDocument, addTotal, getDocument } from "@/lib/firestore"
 import Link from "next/link"
 import { useDispatch, useSelector } from "react-redux"
 import { resetType } from "@/redux/typeSlice"
 import { AppState } from "@/redux/store"
-// export const getStaticPaths = async () => {
-//   const paths = RESULT_PLACE.map((props) => {
-//     return {
-//       params: { id: props.id },
-//     }
-//   })
+export const getStaticPaths = async () => {
+  const paths = RESULT_PLACE.map((props) => {
+    return {
+      params: { id: props.id },
+    }
+  })
 
-//   return {
-//     paths,
-//     fallback: false,
-//   }
-// }
+  return {
+    paths,
+    fallback: false,
+  }
+}
 
-// export const getStaticProps: GetStaticProps = async ({ params }) => {
-//   const res = GetMbtiPlace(String(params?.id))
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const res = GetMbtiPlace(String(params?.id))
 
-//   return {
-//     props: {
-//       place: res,
-//     },
-//   }
-// }
+  return {
+    props: {
+      place: res,
+    },
+  }
+}
 
-const ResultPlace = () => {
+const ResultPlace = ({
+  pageProps,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   const dispatch = useDispatch()
   const mbti = useSelector((state: AppState) => state.types.type).join("")
   const month = new Date().getMonth() + 1
   const date = new Date().getDate()
   const currentDate = month + "-" + date
-  const res = GetMbtiPlace(String(mbti))
+  const res = pageProps.place
 
   const handleClick = async () => {
     const prevData = await getDocument(currentDate, mbti)
@@ -52,14 +54,10 @@ const ResultPlace = () => {
     }
   }
 
-  useEffect(() => {
-    // handleClick()
-  }, [])
-
   return (
     <>
       <PlaceWrapper>
-        <Image src={res?.image} alt="결과 페이지" fill priority quality={100} />
+        <Image src={res.image} alt="결과 페이지" fill priority quality={100} />
         <ButtonWrapper href="/" onClick={() => handleClick()}>
           <Image
             src="/images/logo/completeBtn.svg"
